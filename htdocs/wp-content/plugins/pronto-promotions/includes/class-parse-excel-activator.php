5200d866782ae6d3301b8eea694ddce126aa4d65
Andres Peña Luna <Andres Pea Luna>
2016-04-26 20:28:59 -0300

    db

---
+++ htdocs/wp-content/plugins/pronto-promotions/includes/class-parse-excel-activator.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       
+ * @since      1.0.0
+ *
+ * @package    Parse_Excel
+ * @subpackage Parse_Excel/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Parse_Excel
+ * @subpackage Parse_Excel/includes
+ * @author     Andrés Peña <penalunandres@gmail.com>
+ */
+class Parse_Excel_Activator {
+
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function activate() {
+
+        global $wpdb;
+        $table = $wpdb->prefix . "parse_excel_data";
+        $sql = "CREATE TABLE ".$table." (
+		id INT NOT NULL AUTO_INCREMENT,
+		departamento VARCHAR (512) NOT NULL,
+		rubro VARCHAR (255) NOT NULL,
+		localidad VARCHAR (255) NOT NULL,
+		nombre_comercial VARCHAR (512) NOT NULL,
+		direccion VARCHAR (512) NOT NULL,
+		beneficios VARCHAR (512) NOT NULL,
+  		PRIMARY KEY (id)
+		);";
+
+        require( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+        //$wpdb->query($sql);
+
+    }
+}
