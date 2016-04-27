9f78737edc9e434c511d9400b1decf2103e4393e
Andres Peña Luna <Andres Pea Luna>
2016-04-26 22:43:48 -0300

    xls parsing working and saving in db

---
+++ htdocs/wp-content/plugins/parse-excel/admin/class-parse-excel-admin.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       http://lospibesnotienenpagina.com
+ * @since      1.0.0
+ *
+ * @package    Parse_Excel
+ * @subpackage Parse_Excel/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Parse_Excel
+ * @subpackage Parse_Excel/admin
+ * @author     Andrés <penalunandres@gmail.com>
+ */
+class Parse_Excel_Admin {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Parse_Excel_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Parse_Excel_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/parse-excel-admin.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Parse_Excel_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Parse_Excel_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/parse-excel-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
--- htdocs/wp-content/plugins/parse-excel/parse-excel.php
+++ htdocs/wp-content/plugins/parse-excel/parse-excel.php
@@ -8,47 +8,54 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              
+ * @link              http://lospibesnotienenpagina.com
  * @since             1.0.0
- * @package           parse-excel
+ * @package           Parse_Excel
  *
  * @wordpress-plugin
  * Plugin Name:       Parse Excel
- * Plugin URI:        
- * Description:       Parse data from .xls
+ * Plugin URI:        http://www.notenemospaginadelplugin.com
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Andrés Peña
- * Author URI:        
+ * Author:            Andrés
+ * Author URI:        http://lospibesnotienenpagina.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       Parse_Excel
+ * Text Domain:       parse-excel
  * Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
 
 /**
  * The code that runs during plugin activation.
+ * This action is documented in includes/class-parse-excel-activator.php
  */
 function activate_parse_excel() {
-    require_once plugin_dir_path( __FILE__ ) . 'includes/class-parse-excel-activator.php';
-    Parse_Excel_Activator::activate();
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-parse-excel-activator.php';
+	Parse_Excel_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-parse-excel-deactivator.php
  */
 function deactivate_parse_excel() {
-    require_once plugin_dir_path( __FILE__ ) . 'includes/class-parse-excel-deactivator.php';
-    Parse_Excel_Deactivator::deactivate();
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-parse-excel-deactivator.php';
+	Parse_Excel_Deactivator::deactivate();
 }
 
 register_activation_hook( __FILE__, 'activate_parse_excel' );
 register_deactivation_hook( __FILE__, 'deactivate_parse_excel' );
 
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-parse-excel.php';
 
 /**
  * Begins execution of the plugin.
@@ -61,63 +68,45 @@ register_deactivation_hook( __FILE__, 'deactivate_parse_excel' );
  */
 function run_parse_excel() {
 
-    $plugin = new Parse_Excel();
-    $plugin->run();
+	$plugin = new Parse_Excel();
+	$plugin->run();
 
 }
+
 //menu items
 function parse_excel_menu() {
 
     // Main Menu
     add_menu_page(
-        'Centros Acopio', //page title
-        'Centros Acopio', //menu title
+        'Parse Excel', //page title
+        'Parse Excel', //menu title
         'edit_pages', //capabilities
-        'centros_main', //menu slug
-        'centros_item_create' //function
+        'parse_excel', //menu slug
+        'load_from_excel' //function
     );
 
 
     // add
-    add_submenu_page(
-        'centros_main', //parent slug
-        'Agregar centro', //page title
-        'Agregar centro', //menu title
+    /*add_submenu_page(
+        'parse_excel', //parent slug
+        'Cargar Excel', //page title
+        'Cargar desde excel', //menu title
         'edit_pages', //capability
         'centros_item_create', //menu slug
         'centros_item_create' //function
-    );
+    );*/
 
-    // list
-    add_submenu_page(
-        'centros_main', //parent slug
-        'Listar centros', //page title
-        'Listar centros', //menu title
-        'edit_pages', //capability
-        'centros_list_items', //menu slug
-        'centros_list_items' //function
-    );
-
-    // update
-    add_submenu_page(
-        '', //parent slug
-        'Actualizar centro', //page title
-        'Actualizar centro', //menu title
-        'edit_pages', //capability
-        'centros_update_item', //menu slug
-        'centros_update_item' //function
-    );
 
 }
-//require_once('centros-add-item.php');
+require_once('load-from-excel.php');
 //require_once('centros-update-item.php');
 //require_once('centros-list-items.php');
 //require_once('includes/helpers.php');
 
 add_action('admin_menu','parse_excel_menu');
 
-add_shortcode( 'ParseExcel', 'parseExcel_shortcode' );
-function centrosAcopio_shortcode( $atts )
+add_shortcode( 'ParseExcel', 'parse_excel_shortcode' );
+function parse_excel_shortcode( $atts )
 {
     return parseExcelLogic( $atts );
 }
@@ -125,13 +114,10 @@ function centrosAcopio_shortcode( $atts )
 // THE LOGIC
 function parseExcelLogic( $atts )
 {
-
-    return 'hola';
+	return 'hola';
 }
 // WIDGET
 //require_once( dirname(__FILE__) . "/widget.php");
 
 
 run_parse_excel();
-
-?>
